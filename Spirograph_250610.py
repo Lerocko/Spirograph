65332847e52b0a737e12e7e282d0dc232468a524
Lerocko <L.mz@outlook.com>
2025-10-10 21:05:01 +1300

    Update Spirograph: animated drawing and new parameters

--- Spirograph_250610.py
+++ Spirograph_250610.py
@@ -2,15 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-def generate_points(R = 250, r = 10, d = 5):
+def generate_points(R = 20, r = 19, d = 5):
     k = r / R
     l = d / r
     numerator = round(R - r) # rounding numerator to avoid floating point issues
     denominator = round(r) # rounding denominator to avoid floating point issues
     g = math.gcd(numerator, denominator) # greatest common divisor
-    q = denominator // g # number of rotations
-    xs = []
-    ys = []
+    q = (denominator // g) # number of rotations
+    xs, ys = [], []
     for theta in range(0, 360 * q, 5):
         theta_rad = np.radians(theta)
         x = R*((1-k)*np.cos(theta_rad) + l*k*np.cos(((1-k)/k)*theta_rad))
@@ -21,10 +20,18 @@ def generate_points(R = 250, r = 10, d = 5):
 
 def main():
     xs, ys = generate_points()
-    plt.figure(figsize=(8, 8))
-    plt.plot(xs, ys)
-    plt.axis('equal')
-    plt.title('Hypotrochoid')
+    plt.ion()
+    fig, ax = plt.subplots(figsize=(8, 8))
+    ax.set_aspect('equal')
+    ax.set_xlim(min(xs)-10, max(xs)+10)
+    ax.set_ylim(min(ys)-10, max(ys)+10)
+    ax.set_title('Hypotrochoid')
+    line, = ax.plot([], [], color='blue')
+    
+    for i in range(len(xs)):
+        line.set_data(xs[:i], ys[:i])
+        plt.pause(0.001)
+    plt.ioff()
     plt.show()
         
         
